47864710b480e488d56e990581de146da9d8bbc3
Felipe <felipe.viera@mayor.cl>
2024-12-19 21:11:05 -0300

    2do update

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 from fastapi import FastAPI 
+
+# Crea unas instancia de la aplicación
+app = FastAPI()
+
+# Ruta raíz (GET /)
+@app.get("/")
+def read_root():
+    return {"message": "Hello, FastAPI!"}
