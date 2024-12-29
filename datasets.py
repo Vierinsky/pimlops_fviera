18eb9d33b9b0a7a2dae51e1de6961592ac8d645d
Felipe <felipe.viera@mayor.cl>
2024-12-29 14:15:18 -0300

    2.22 intento API: En datasets.py se corrige path de movie_actors.csv y movie_directors.csv

--- datasets.py
+++ datasets.py
@@ -11,16 +11,17 @@ movies_score_path = os.path.join(base_path, "datasets", "movies_score.csv")
 movies_actors_path = os.path.join(base_path, "datasets", "movies_actors.csv")
 movies_directors_path = os.path.join(base_path, "datasets", "movies_directors.csv")
 
-# Se carga el dataset en una variable global
+# Se cargan los dataset en una variable global
 try:
     movies_date = pd.read_csv(movies_date_path)
     movies_details = pd.read_csv(movies_details_path)
     movies_score = pd.read_csv(movies_score_path)
+    movies_actors = pd.read_csv(movies_actors_path)
+    movies_directors = pd.read_csv(movies_directors_path)
 except FileNotFoundError as e:
     raise Exception(f"Error al cargar los datasets: {e}")
 
 # Se prepara el dataset combinado
-
 try:
     movies_pop_date = pd.merge(
         pd.merge(movies_details, movies_date, on="movie_id", how="inner"),
